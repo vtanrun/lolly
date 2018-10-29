46b6c78bca077d42d7aef44194600ca9dadc03bd
xiaozhuox <mrxzx@wwsg18.top>
2018-10-29 15:03:54 +0800

    Commit Code

--- config/site.php
+++ config/site.php
@@ -7,4 +7,5 @@ return array(
     "Local_Path" => "/LollyPlus/public",        //网站访问地址
     "Site_Index" => "index",                    //默认路由名
     "Static_Path" => [],                        //静态文件地址
+    "Domain_Location" => '_PUBLIC_'             //位置值(用于资源引入)
 );
